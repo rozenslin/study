b83a938e85fc0a405c2f515df52e8b8febaf5d6b
Rozen Lin <rozenslin@gmail.com>
2021-11-02 16:30:19 -0700

    update asm indent

--- asm/power.s
+++ asm/power.s
@@ -2,7 +2,7 @@
     .section .data
 msg:
     .ascii    "Hello, world!\n"   # our dear string
-	len = . - msg                 # length of our dear string
+    len = . - msg                 # length of our dear string
 
     .section .text
     .globl _start
@@ -21,11 +21,11 @@ _start:
     addl  %eax, %ebx
     pushl %ebx
 
-    movl    $len,   %edx           # third argument: message length
-	movl    $msg,   %ecx           # second argument: pointer to message to write
-	movl    $1, %ebx             # first argument: file handle (stdout)
-	movl    $4, %eax             # system call number (sys_write)
-	int     $0x80               # call kernel
+    movl    $len,   %edx   # third argument: message length
+    movl    $msg,   %ecx   # second argument: pointer to message to write
+    movl    $1,     %ebx   # first argument: file handle (stdout)
+    movl    $4,     %eax   # system call number (sys_write)
+    int     $0x80          # call kernel
 
     popl  %ebx
     movl  $1,   %eax
