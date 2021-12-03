291100f2f3955932dc79995e6a424b3610d3cc67
Rozen Lin <rozenslin@gmail.com>
2021-12-02 19:41:16 -0800

    update string.cpp

--- cpp/string.cpp
+++ cpp/string.cpp
@@ -5,174 +5,140 @@ const int MAX_LEN = 256;
 class String
 {
     private:
-        const int m_max;
+        const size_t m_max;
         char* m_str;
         size_t m_len;
 
     public:
         // def ctor
-        String();
-        // ctor
-        String(const char* str);
+        String(const char*, const size_t);
         // copy ctor
-        String(const String& str);
-        // destor
+        String(const String &);
+        // detor
         ~String();
 
         // ops
-        String& operator=(const String& str);
-        String& operator+(const String& str);
-        String& operator=(const char* str);
+        String& operator=(const String&);
+        String& operator=(const char*);
+        String& operator+(const String&);
 
         // I/O
-        friend ostream& operator<<(ostream& os, const String& str);
-        friend istream& operator>>(istream& is, String& str);
+        friend bool operator==(const String&, const String&);
+        friend ostream& operator<<(ostream&, const String &);
+        friend istream& operator>>(istream&, const String &);
 };
 
-String::String() : m_max(MAX_LEN)
+// def ctor
+String::String(const char* s = NULL, const size_t max = MAX_LEN): m_max(max)
 {
-    m_len = 0;
-    m_str = new char[1];
-    m_str[0] = '\0';
+    if (!s)
+        m_len = 0;
+    else 
+        m_len = strlen(s);
+
+    if (m_len > m_max)
+    {
+        cerr << "String len exceeds the max len of: " << m_max << endl;
+        return;
+    }
+
+    m_str = new char[m_len + 1];
+    strncpy(m_str, s, m_len);
+    m_str[m_len] = '\0';
+    cout << "ctor\t ---- string created for :'" << m_str << "' at addr: " << (void*)m_str << endl;
 }
 
 String::~String()
 {
-    cout << "string: " << m_str << " at addr: " << (void*) m_str  << " freed " <<endl;
-    delete [] m_str;
+    cout << "detor\t ---- string freed   for :'" << m_str << "' at addr: " << (void*)m_str << endl;
+    if (m_str)
+        delete [] m_str;
 }
 
-String::String(const char* str) : m_max(MAX_LEN)
+// cp ctor
+String::String(const String& s): m_max(s.m_max)
 {
-    if (!str || !strlen(str) || strlen(str) > m_max)
-    {
-        m_str = new char[1];
-        m_str[0] = '\0';
-        m_len = 0;
-        return;
-    }
-
-    m_len = strlen(str);
+    m_len = s.m_len;
     m_str = new char[m_len + 1];
-    strncpy(m_str, str, m_len);
+    strncpy(m_str, s.m_str, m_len);
     m_str[m_len] = '\0';
+    cout << "cp ctor\t ---- string created for :'" << m_str << "' at addr: " << (void*)m_str << endl;
 }
 
-String::String(const String& str) : m_max(MAX_LEN)
+String& String::operator=(const String& s)
 {
-    if (!str.m_len)
-    {
-        m_str = new char[1];
-        m_str[0] = '\0';
-        m_len = 0;
-        return;
-    }
-
-    m_len = str.m_len;
-    m_str = new char[m_len + 1];
-    strncpy(m_str, str.m_str, m_len);
-    m_str[m_len] = '\0';
-}
-
-String& String::operator=(const String& str)
-{
-    if (this == &str)
+    if (this == &s)
         return *this;
 
-    cout << "string: " << m_str << " at addr: " << (void*) m_str  << " freed " <<endl;
+    if (s.m_len > m_max)
+        return *this;
+
+    cout << "op =\t ---- string freed   for :'" << m_str << "' at addr: " << (void*)m_str << endl;
     delete [] m_str;
 
-    if (!str.m_len)
-    {
-        m_str = new char[1];
-        m_str[0] = '\0';
-        m_len = 0;
-        return *this;
-    }
-
-    m_len = str.m_len;
+    m_len = s.m_len;
     m_str = new char[m_len + 1];
-    strncpy(m_str, str.m_str, m_len);
+    strncpy(m_str, s.m_str, m_len);
     m_str[m_len] = '\0';
-
+    cout << "op =\t ---- string created for :'" << m_str << "' at addr: " << (void*)m_str << endl;
     return *this;
 }
 
-String& String::operator=(const char* str)
+String& String::operator=(const char* s)
 {
-    if (!str || !strlen(str) || strlen(str) > m_max)
-    {
-        m_str = new char[1];
-        m_str[0] = '\0';
-        m_len = 0;
+    if (!s)
         return *this;
-    }
 
-    cout << "string: " << m_str << " at addr: " << (void*) m_str  << " freed " <<endl;
-    delete [] m_str;
-
-    m_len = strlen(str);
-    m_str = new char[m_len + 1];
-    strncpy(m_str, str, m_len);
-    m_str[m_len] = '\0';
-
+    cout << "op =\t ---- string will be created for :'" << s << "'" << endl;
+    *this = String(s);
     return *this;
 }
 
-ostream& operator<<(ostream& os, const String& str)
+ostream& operator<<(ostream & os, const String & s)
 {
-    os << str.m_str;
+    os << s.m_str;
     return os;
 }
 
-istream& operator>>(istream& is, String& str)
+istream& operator>>(istream& is, String& s)
 {
-    is.clear();
+    char * buf = new char[MAX_LEN];
+    memset(buf, 0, MAX_LEN);
 
-    char buf[MAX_LEN+1];
-    memset(buf, 0, MAX_LEN+1);
+    is.clear();
     if (is.getline(buf, MAX_LEN))
-    {
-        str = buf;
-    }
+        s = buf;
+
+    is.clear();
 
     return is;
 }
 
-void callme1(String & rsb) {
-    cout << "String passed by reference:"<<endl;
-    cout << rsb << endl;
-}
-
-void callme2(String sb) {
-    cout << "String passed by value:\n";
-    cout << sb << endl;
-}
-
 int main()
 {
-    String a = String("ahooooooooooooooooooooo");
-    String b("wahoooooooooooooooooo");
-    String c(a);
-    cout << a << " - " << b << " - " << c  << " - " <<endl;
-    // c.operator=("") 
-    c = "mahoooooo";
-    cout << a << " - " << b << " - " << c  << " - " <<endl;
-    // c.operator=(a.operator=(b))
-    c = a = b;
-    cout << a << " - " << b << " - " << c  << " - " <<endl;
-    c = c;
+    // ctor
+    String a = String("ahooooooooooooooooooooo A");
+    // ctor
+    String b("ahooooooooooooooooooooo B");
+    // ctor
+    String c = "ahooooooooooooooooooooo C";
+    // def ctor
+    String d;
+    // copy ctor
+    String e(a);
+    // op = ctor
+    e = b;
+    // op = ctor
+    e = "ahooooooooooooooooooooo E";
+    // no op
+    e = e;
+
+    cout << endl;
+    operator<<(cout, a) << endl << endl;
 
     cout << "what saying:" << endl;
-    cin >> c;
-    cout << "said: " << c << endl;
-
-    cout << "what saying again:" << endl;
-    cin >> c;
-    cout << "said: " << c << endl;
-
-    callme1(c);
-    callme2(c);
+    cin >> d;
+    cout << "said: " << d << endl;
 
     return 0;
 }
