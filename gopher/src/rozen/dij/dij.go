670202179fa39dabbc18df885667f227d88af614
Rozen Lin <rozenslin@gmail.com>
2022-03-17 16:02:18 -0700

    update dij.go

--- gopher/src/rozen/dij/dij.go
+++ gopher/src/rozen/dij/dij.go
@@ -24,9 +24,9 @@ type Edge struct {
 }
 
 // XXX dup for node.id and k, but the lazy way...
-func gen_nodes(node_list []string) map[string]*Node {
+func gen_nodes(cities []string) map[string]*Node {
     nodes := make(map[string]*Node)
-    for _, v := range node_list {
+    for _, v := range cities {
         node := &Node{v, DIST_MAX, nil}
         if v == "a" {
             // start point
@@ -39,38 +39,36 @@ func gen_nodes(node_list []string) map[string]*Node {
 }
 
 // get the node from nodes that has smallest 'distance', and remove it from nodes.
-func get_smallest(nodes *map[string]*Node) *Node {
-    var smallest *Node = nil
-    for _, v := range *nodes {
-        if smallest == nil {
-            smallest = v
-        } else if smallest.dist > v.dist {
-            smallest = v
+func get_smallest(nodes map[string]*Node) *Node {
+    var s *Node = nil
+    for _, v := range nodes {
+        if s == nil || s.dist > v.dist {
+            s = v
         }
     }
 
-    delete(*nodes, smallest.id)
-    fmt.Printf("calc current node: %s, dist to start node:%d\n", smallest.id, smallest.dist)
-    return smallest
+    delete(nodes, s.id)
+    fmt.Printf("calc current node: %s, dist to start node:%d\n", s.id, s.dist)
+    return s
 }
 
 // get the nodes connected to current one
 // calc their smallest dist to start node and update
 func calc_conn_nodes(nodes map[string]*Node, node *Node, roads[]Edge) {
-    var conn_id string
+    var id string
 
-    for _, e := range roads {
-        if e.id1 == node.id {
-            conn_id = e.id2
-        } else if e.id2 == node.id {
-            conn_id = e.id1
+    for _, r := range roads {
+        if r.id1 == node.id {
+            id = r.id2
+        } else if r.id2 == node.id {
+            id = r.id1
         } else {
             continue
         }
 
         // if the connected node still in map, update its dist
-        if n, ok := nodes[conn_id]; ok {
-            dist := e.dist + node.dist
+        if n, ok := nodes[id]; ok {
+            dist := r.dist + node.dist
             if dist < n.dist {
                 n.dist = dist
                 n.prev = node
@@ -91,16 +89,15 @@ func get_path(n *Node) {
 // total time O(E*V^2)
 // if get_smallest using min prior queue, then time will be
 // O(E*LogV) (E >> V)
-func do_dij(nodes *map[string]*Node, roads []Edge) {
-    for len(*nodes) != 0 {
+func do_dij(nodes map[string]*Node, roads []Edge) {
+    for len(nodes) != 0 {
         node := get_smallest(nodes)
-        calc_conn_nodes(*nodes, node, roads)
+        calc_conn_nodes(nodes, node, roads)
     }
 }
 
-
 func main() {
-    node_list := []string{"a", "b", "c", "d", "e", "f"}
+    cities := []string{"a", "b", "c", "d", "e", "f"}
     roads := []Edge {
         {"a", "b", 3},
         {"a", "c", 3},
@@ -113,11 +110,11 @@ func main() {
         {"e", "f", 1},
     }
 
-    nodes := gen_nodes(node_list)
+    nodes := gen_nodes(cities)
 
     node := nodes["f"]
 
-    do_dij(&nodes, roads)
+    do_dij(nodes, roads)
 
     get_path(node)
 }
